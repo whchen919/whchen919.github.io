7ac3a08b8623b894202626fac29227f8eb15d94b
whchen919 <ts00389963@yahoo.com.tw>
2025-10-09 04:53:59 +0000

    Deploying to gh-pages from @ whchen919/whchen919.github.io@cd57ed73503889eafb975a441668ac9cf1718d03 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "whchen919/portfolio",
+    "data-repo": "whchen919/whchen919.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
