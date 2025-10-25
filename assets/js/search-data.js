db17f489beb59b2eebe02703ed05afc181f22650
whchen919 <ts00389963@yahoo.com.tw>
2025-10-25 18:10:13 +0000

    Deploying to gh-pages from @ whchen919/whchen919.github.io@b792e8ff3d83735aa95f006a864c4cc6162e83d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,12 +52,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "projects-ship-airwake-rotor-aerodynamic-interactions",
+      },{id: "projects-ship-airwake-rotor-aerodynamic-interactions",
           title: 'Ship-Airwake-Rotor Aerodynamic Interactions',
           description: "",
           section: "Projects",handler: () => {
